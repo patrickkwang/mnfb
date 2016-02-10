670c704c15d09c278051698e98d9a0b746603557
Patrick Wang <patrick@Patricks-MacBook-Pro.local>
2016-02-10 12:41:44 -0500

    again

--- mirroredNormalFisherBinghamDist.m
+++ mirroredNormalFisherBinghamDist.m
@@ -3,7 +3,7 @@ classdef mirroredNormalFisherBinghamDist < normalFisherBinghamDist
 
 methods
 	function obj = mirroredNormalFisherBinghamDist(varargin)
-		obj = prtUtilAssignStringValuePairs(obj,varargin{:});
+		obj = utilAssignStringValuePairs(obj,varargin{:});
 		obj = obj.sortEigenvectors();
 		
 		% compute normalization constant
@@ -26,7 +26,7 @@ methods
     end
     
     % finish up
-		val = 1/obj.C*exp(expt);
+		val = 1/obj.C*exp(expt)';
   end
   
   function mnfb = conditional(obj,d,points)
@@ -39,7 +39,7 @@ methods
     % fix variable counts
     dCond = obj.d;
     for i = 1:length(d)
-      j = find(d<cumsum(obj.d))-1;
+      j = find(d(i)<=cumsum(obj.d),1,'first');
       dCond(j) = dCond(j)-1;
     end
 
@@ -48,15 +48,50 @@ methods
     aCond = obj.a(g) + 2*obj.B(g,d)*points';
     BCond = obj.B(g,g);
     
-    mnfb = mirroredNormalFisherBinghamDist('a',aCond,'B',BCond,'d',dCond);
-  end
+    mnfb = utilAssignStringValuePairs(obj,'d',dCond,'a',aCond,'B',BCond);
+	end
+	
+	function mnfb = marginal(obj,d)
+    % d should be an nx1 array of indices
+    %
+    % We should check to make sure we're not trying to marginalize over a
+    % subset of linked directional variables. Do this.
+    
+		warning('this only works if there are no Fisher components')
+		
+    % fix variable counts
+    dMarg = obj.d;
+    for i = 1:length(d)
+      j = find(d(i)<=cumsum(obj.d),1,'first');
+      dMarg(j) = dMarg(j)-1;
+    end
+		
+    % break into block matrix
+		d = sort(d);
+    g = setdiff(1:sum(obj.d),d);
+		mode = [-1/2*(obj.B(1:obj.d(1),1:obj.d(1))\obj.a(1:obj.d(1)));zeros(sum(obj.d(2:end)),1)];
+		modeMarg = mode(g);
+		
+		% compute second moment
+		n = 1000;
+		t = linspace(0,2*pi,n+1)'; t = t(1:end-1);
+		x = [cos(t),sin(t)];
+		b = normalFisherBinghamDist('d',[0,2],'B',obj.B(d,d),'a',[0;0]);
+		p = b.pdf(x);
+		m = x'*bsxfun(@times,x,p)*(t(2)-t(1));
+		
+    BMarg = obj.B(g,g)+2*obj.B(g,d)*m*obj.B(d,g);
+		
+    mnfb = utilAssignStringValuePairs(obj,'d',dMarg,'B',BMarg,'mu',modeMarg);
+		
+	end
   
   function bingham = approximate(obj)
     x = obj.mode;
     B = (-obj.a*x' - x*obj.a' - obj.a'*x*(eye(2) - 3*(x*x'))...
       +2*obj.B - 4*obj.B*(x*x') - 4*(x*x')*obj.B - 2*x'*obj.B*x*(eye(2) - 4*(x*x')))/2;
     [V,Z] = eig(B);
-    bingham = mirroredNormalFisherBinghamDist('Z',diag(Z),'V',V,'d',[0,2],'a',[0;0]);
+    bingham = utilAssignStringValuePairs(obj,'d',[0,2],'B',B,'a',[0;0]);
   end
   
   function px = hs(obj)
@@ -64,8 +99,15 @@ methods
     % infer the angle from the eigenvectors (hue)
     % take the first eigenvalue to represent the concentration (saturation)
     
-    hs(1) = atan2(obj.V(2,2),obj.V(1,2))*2/(2*pi);
-    hs(2) = 1-exp(obj.Z(1));
+		ang = atan2(obj.V(2,2),obj.V(1,2))*2;
+		while ang>=2*pi
+			ang = ang-2*pi;
+		end
+		while ang<0
+			ang = ang+2*pi;
+		end
+    px(1) = ang/(2*pi);
+    px(2) = 1-exp(obj.Z(1));
     
   end
 		
@@ -73,21 +115,68 @@ end
 
 methods (Static)
   function test()
-    Z = [-2, -10, 0];
-    ry = -pi/4;
-    rz = pi/6; % rotates about z-axis
-    Ry = RotationMatrix.Rz(ry);
-    Rz = RotationMatrix.Rx(rz);
-    V = Rz*Ry;
+%     Z = [-2, -10, 0];
+%     ry = -pi/4;
+%     rz = pi/6; % rotates about z-axis
+%     Ry = RotationMatrix.Rz(ry);
+%     Rz = RotationMatrix.Rx(rz);
+%     V = Rz*Ry;
+%     mnfb = mirroredNormalFisherBinghamDist('V',V,...
+%       'Z',Z,...
+%       'mu',[10,0,0]);
+%     figure(1), plot3(mnfb)
+%     mnfbCond = mnfb.conditional(1,9);
+%     figure(2), plot3(mnfbCond)
+%     mnfbAppx = mnfbCond.approximate;
+%     figure(3), plot3(mnfbAppx)
+%     mnfbMarg = mnfb.marginal([2,3]);
+%     figure(4), plot3(mnfbMarg)
+%     % plot with colors
+% 		mu = -1/2*(mnfbMarg.B\mnfbMarg.a);
+% 		zs = mu+1/sqrt(-mnfbMarg.B)*linspace(-3,3,100)';
+% 		v = mnfbMarg.pdf(zs);
+% 		for i = 1:100
+% 			mnfbCond = mnfb.conditional(1,zs(i));
+% 			mnfbAppx = mnfbCond.approximate;
+% 			hs(i,:) = mnfbAppx.hs;
+% 		end
+% 		hsv = cat(2,hs,v/max(v));
+% 		rgb = hsv2rgb(permute(hsv,[1,3,2]));
+% 		figure(5), imagesc(rgb)
+		
+    Z = [-2, -2, -10, 0];
+		Ry = RotationMatrix('euler',[2.1847 0.9425 3.6825]); %rand(1,3)
+		Ry = blkdiag(Ry.R,1);
+		Rz = RotationMatrix.Rx(pi/3);
+		Rz = blkdiag(1,Rz);
+		V = Rz*Ry;
     mnfb = mirroredNormalFisherBinghamDist('V',V,...
       'Z',Z,...
-      'mu',[10,0,0]);
-    figure(1), plot3(mnfb)
-    mnfbCond = mnfb.conditional(1,10.5);
+      'mu',[10,10,0,0],...
+			'd',[2,2]);
+    mnfbCond = mnfb.conditional(1,10);
     figure(2), plot3(mnfbCond)
-    mnfbAppx = mnfbCond.approximate;
-    figure(3), plot3(mnfbAppx)
-    keyboard
+    mnfbMarg = mnfb.marginal([3,4]);
+    figure(4), plot3(mnfbMarg)
+    % plot with colors
+		mu = -1/2*(mnfbMarg.B\mnfbMarg.a);
+		x = mu(1)+1/sqrt(-mnfbMarg.B(1,1))*linspace(3,-3,100)';
+		y = mu(2)+1/sqrt(-mnfbMarg.B(2,2))*linspace(-3,3,100)';
+		[X,Y] = meshgrid(x,y);
+		samples = [X(:),Y(:)];
+		v = mnfbMarg.pdf(samples);
+		hs = nan(size(samples,1),2);
+		for i = 1:size(samples,1)
+			mnfbCond = mnfb.conditional([1,2],samples(i,:));
+			mnfbAppx = mnfbCond.approximate;
+			hs(i,:) = mnfbAppx.hs;
+			if mod(i,10)==0
+				fprintf('%d of %d\n',i,size(samples,1));
+			end
+		end
+		hsv = cat(2,hs,v/max(v));
+		rgb = hsv2rgb(reshape(hsv,[100,100,3]));
+		figure(5), imagesc(rgb)
   end
 end
 
--- normalFisherBinghamDist.m
+++ normalFisherBinghamDist.m
@@ -5,9 +5,6 @@ properties
   % dimension vector d = [n,m1,m2,...]
   % first element is the number of linear dimensions (it may be zero)
   d = [1,2];
-  
-	% concentration matrix
-	Z = [-1e1,-1e1,0];
 
 	% orientation matrix
 	V = eye(3);
@@ -21,7 +18,13 @@ end
 properties (Dependent)
   B
   mu
-end  
+	Z
+end
+
+properties (Hidden, SetAccess=private)
+	% concentration matrix
+	Z_ = [-1e1,-1e1,0];
+end
 
 methods
 	function obj = normalFisherBinghamDist(varargin)
@@ -36,9 +39,12 @@ methods
 	function obj = sortEigenvectors(obj)
     for i=2:length(obj.d)
       inds = sum(obj.d(1:i-1))+1:sum(obj.d(1:i));
-      [~,order] = sort(obj.Z(inds));
+      [~,order] = sort(obj.Z_(inds));
       obj.V(:,inds) = obj.V(:,inds(order));
-      obj.Z(inds) = obj.Z(inds(order))-max(obj.Z(inds));
+      obj.Z_(inds) = obj.Z_(inds(order));
+			if obj.d(1)==0 % only if there are no linear components
+				obj.Z_(inds) = obj.Z_(inds)-max(obj.Z_(inds));
+			end
     end
 	end
 	
@@ -52,7 +58,7 @@ methods
     end
     
     % finish up
-		val = 1/obj.C*exp(expt);
+		val = 1/obj.C*exp(expt)';
   end
   
   function nfb = conditional(obj,d,points)
@@ -107,7 +113,7 @@ methods
     thetas = thetas(1:nThetas);
     
     % the follow allows for plotting (Fisher-)Bingham distributions, too
-    if obj.d(1)
+    if obj.d(1) && obj.d(2)
       nZs = 100;
       zs = 10+linspace(-3,3,nZs)/sqrt(-2*obj.Z(1)); % assumes mean of 10
       [T,X] = meshgrid(thetas,zs);
@@ -123,6 +129,29 @@ methods
       xlabel('q_1')
       ylabel('q_2')
       zlabel('x')
+		elseif obj.d(1)==1 % 1-D Gaussian
+			% generate sample points
+			x = -1/2/obj.B*obj.a+1/sqrt(-obj.B)*linspace(-3,3,1000)';
+
+			% evaluate pdf at sample points
+			p = obj.pdf(x);
+
+			% plot
+			plot(x,p)
+			xlabel('x')
+		elseif obj.d(1)==2 % 2-D Gaussian
+			% generate sample points
+			mu = -1/2*(obj.B\obj.a);
+			x = mu(1)+1/sqrt(-obj.B(1,1))*linspace(-3,3,1000)';
+			y = mu(2)+1/sqrt(-obj.B(2,2))*linspace(-3,3,1000)';
+			[X,Y] = meshgrid(x,y);
+
+			% evaluate pdf at sample points
+			p = obj.pdf([X(:),Y(:)]);
+			
+			% display
+			imagesc(x,y,reshape(p,size(X)))
+			
     else
       x = [cos(thetas(:)),sin(thetas(:))];
       
@@ -166,38 +195,126 @@ methods
   
   function obj = set.a(obj,val)
     obj.a = val(:);
-  end
+	end
+	
+	function obj = set.Z(obj,val)
+		obj.Z_ = val;
+		obj = obj.sortEigenvectors();
+	end
+	
+	function val = get.Z(obj)
+		val = obj.Z_;
+	end
   
   function mode_hat = mode(obj)
-    % find the mean of the Gaussian
-    % we can only know that the mean lies somewhere on the major axis
-    % let's say that it's on the x-axis...
-    mu_hat = [-1/2*obj.a(1)/obj.V(1,1)^2/obj.Z(1);0];
-    % to derive this, expand the expression a = -2 B mu
-    % assuming that mu(2)==0
+		% find the major axis of the Gaussian
+		m = obj.V(:,2);
+		
+		assert(m(1)~=0,'this does not work')
+			
+		if m(1)~=0
+			% find the mean of the Gaussian
+% 			mu_hat = [-1/2*obj.a(1)/obj.V(1,1)^2/obj.Z(1);0];
+			mu_hat = (-2*obj.B*[1;obj.V(2,1)/obj.V(1,1)])\obj.a*[1;obj.V(2,1)/obj.V(1,1)];
+			% to derive this, expand the expression a = -2 B mu
+			% assuming that mu(2)==m2/m2*mu(1) (it's on the Gaussian's minor axis)
+
+			% find the intersection of the line (y-mu2)/(x-mu1)=m(2)/m(1) with the
+			% unit circle x^2+y^2=1 (y = +-sqrt(1-x^2)): http://mathworld.wolfram.com/Circle-LineIntersection.html
+			x1 = mu_hat(1)+m(1);
+			x2 = mu_hat(1);
+			y1 = mu_hat(2)+m(2);
+			y2 = mu_hat(2);
+			dx = x2-x1;
+			dy = y2-y1;
+			dr = sqrt(dx^2+dy^2);
+			D = x1*y2-x2*y1;
+			
+			mode_hat = {};
+			if dr^2-D^2>=0 % at least one intersection
+				mode_hat = cat(1,mode_hat,{[(D*dy+sign(dy)*dx*sqrt(dr^2-D^2))/dr^2;...
+					(-D*dx+abs(dy)*sqrt(dr^2-D^2))/dr^2]});
+			end
+			if dr^2-D^2>0 % strictly - two intersections
+				mode_hat = cat(1,mode_hat,{[(D*dy-sign(dy)*dx*sqrt(dr^2-D^2))/dr^2;...
+					(-D*dx-abs(dy)*sqrt(dr^2-D^2))/dr^2]});
+			end
+			
+			% and the minor axis
+			m = obj.V(:,1);
+			x1 = mu_hat(1)+m(1);
+			x2 = mu_hat(1);
+			y1 = mu_hat(2)+m(2);
+			y2 = mu_hat(2);
+			dx = x2-x1;
+			dy = y2-y1;
+			dr = sqrt(dx^2+dy^2);
+			D = x1*y2-x2*y1;
+			if dr^2-D^2>=0 % at least one intersection
+				mode_hat = cat(1,mode_hat,{[(D*dy+sign(dy)*dx*sqrt(dr^2-D^2))/dr^2;...
+					(-D*dx+abs(dy)*sqrt(dr^2-D^2))/dr^2]});
+			end
+			if dr^2-D^2>0 % strictly - two intersections
+				mode_hat = cat(1,mode_hat,{[(D*dy-sign(dy)*dx*sqrt(dr^2-D^2))/dr^2;...
+					(-D*dx-abs(dy)*sqrt(dr^2-D^2))/dr^2]});
+			end
+
+% 			% here are the coefficients of the resulting quadratic equation
+% 			a_ = 1+(m(2)/m(1)).^2;
+% 			b_ = -2*(m(2)/m(1)).^2*mu_hat(1);
+% 			c_ = (m(2)/m(1)).^2*mu_hat(1).^2-1;
+% 			
+% 			if b_^2-4*a_*c_<0
+% 				% try the minor axis
+% 				m = obj.V(:,1);
+% 				a_ = 1+(m(2)/m(1)).^2;
+% 				b_ = -2*(m(2)/m(1)).^2*mu_hat(1);
+% 				c_ = (m(2)/m(1)).^2*mu_hat(1).^2-1;
+% 			end
     
-    % find the major axis of the Gaussian
-    m = -obj.V(:,2);
+% 			% find the four solutions to this quadratic equation
+% 			mode_hatX = (-b_+sqrt(b_^2-4*a_*c_))/(2*a_);
+% 			mode_hatY = sqrt(1-mode_hatX^2);
+% 			mode_hat{1} = [mode_hatX;mode_hatY];
+% 			mode_hatY = -sqrt(1-mode_hatX^2);
+% 			mode_hat{3} = [mode_hatX;mode_hatY];
+% 			mode_hatX = (-b_-sqrt(b_^2-4*a_*c_))/(2*a_);
+% 			mode_hatY = sqrt(1-mode_hatX^2);
+% 			mode_hat{2} = [mode_hatX;mode_hatY];
+% 			mode_hatY = -sqrt(1-mode_hatX^2);
+% 			mode_hat{4} = [mode_hatX;mode_hatY];
+			
+% 			if ~isreal(mode_hatX)
+% 				keyboard
+% 			end
+		else
+			% find the mean of the Gaussian
+			% we can only know that the mean lies somewhere on the major axis
+			% let's say that it's on the y-axis...
+			mu_hat = [0;-1/2*obj.a(2)/obj.V(2,1)^2/obj.Z(1)];
+			% to derive this, expand the expression a = -2 B mu
+			% assuming that mu(2)==0
+
+			% find the intersection of the line (y-mu2)/(x-mu1)=m(2)/m(1) with the
+			% unit circle x^2+y^2=1 (x = +-sqrt(1-y^2)):
+			% 
+			% here are the coefficients of the resulting quadratic equation
+			a_ = 1+(m(2)/m(1)).^2;
+			b_ = -2*mu_hat(2).^2;
+			c_ = mu_hat(2).^2-(m(2)/m(1)).^2;
     
-    % find the intersection of the line (y-mu2)/(x-mu1)=m(2)/m(1) with the
-    % unit circle x^2+y^2=1 (y = +-sqrt(1-x^2)):
-    % 
-    % here are the coefficients of the resulting quadratic equation
-    a_ = 1+(m(2)/m(1)).^2;
-    b_ = -2*(m(2)/m(1)).^2*mu_hat(1);
-    c_ = (m(2)/m(1)).^2*mu_hat(1).^2-1;
-    
-    % find the four solutions to this quadratic equation
-    mode_hatX = (-b_+sqrt(b_^2-4*a_*c_))/(2*a_);
-    mode_hatY = sqrt(1-mode_hatX^2);
-    mode_hat{1} = [mode_hatX;mode_hatY];
-    mode_hatY = -sqrt(1-mode_hatX^2);
-    mode_hat{3} = [mode_hatX;mode_hatY];
-    mode_hatX = (-b_-sqrt(b_^2-4*a_*c_))/(2*a_);
-    mode_hatY = sqrt(1-mode_hatX^2);
-    mode_hat{2} = [mode_hatX;mode_hatY];
-    mode_hatY = -sqrt(1-mode_hatX^2);
-    mode_hat{4} = [mode_hatX;mode_hatY];
+			% find the four solutions to this quadratic equation
+			mode_hatY = (-b_+sqrt(b_^2-4*a_*c_))/(2*a_);
+			mode_hatX = sqrt(1-mode_hatY^2);
+			mode_hat{1} = [mode_hatX;mode_hatY];
+			mode_hatX = -sqrt(1-mode_hatY^2);
+			mode_hat{3} = [mode_hatX;mode_hatY];
+			mode_hatY = (-b_-sqrt(b_^2-4*a_*c_))/(2*a_);
+			mode_hatX = sqrt(1-mode_hatY^2);
+			mode_hat{2} = [mode_hatX;mode_hatY];
+			mode_hatX = -sqrt(1-mode_hatY^2);
+			mode_hat{4} = [mode_hatX;mode_hatY];
+		end
     
     % pick the intersection with the greatest likelihood
     llhs = obj.pdf(cat(2,mode_hat{:})');
