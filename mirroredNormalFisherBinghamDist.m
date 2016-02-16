c9e010a8d6a01b6a1296ac63069aff24e0faaa75
Patrick Wang <patrickelvin@gmail.com>
2016-02-16 11:00:37 -0500

    fixed display bug, others

--- mirroredNormalFisherBinghamDist.m
+++ mirroredNormalFisherBinghamDist.m
@@ -16,75 +16,6 @@ methods
     
     val = pdf@normalFisherBinghamDist(obj,points);
   end
-	
-	function mnfb = marginal(obj,d)
-    % d should be an nx1 array of indices
-    %
-    % We should check to make sure we're not trying to marginalize over a
-    % subset of linked directional variables. Do this.
-    
-		warning('this only works if there are no Fisher components')
-		
-    % fix variable counts
-    dMarg = obj.d;
-    for i = 1:length(d)
-      j = find(d(i)<=cumsum(obj.d),1,'first');
-      dMarg(j) = dMarg(j)-1;
-    end
-		
-    % break into block matrix
-		d = sort(d);
-    g = setdiff(1:sum(obj.d),d);
-		mode = [-1/2*(obj.B(1:obj.d(1),1:obj.d(1))\obj.a(1:obj.d(1)));zeros(sum(obj.d(2:end)),1)];
-		modeMarg = mode(g);
-		
-		% compute second moment
-		n = 1000;
-		t = linspace(0,2*pi,n+1)'; t = t(1:end-1);
-		x = [cos(t),sin(t)];
-		b = mirroredNormalFisherBinghamDist('d',[0,2],'B',obj.B(d,d),'a',[0;0]);
-		p = b.pdf(x);
-		m = x'*bsxfun(@times,x,p)*(t(2)-t(1));
-		
-    BMarg = obj.B(g,g)+2*obj.B(g,d)*m*obj.B(d,g);
-		
-    mnfb = mirroredNormalFisherBinghamDist('d',dMarg,'B',BMarg,'mu',modeMarg);
-		
-  end
-  
-  function x = sample(obj,n,burnin)
-    if nargin<3
-      burnin = 100;
-    end
-    
-    addnoise = @(x,stdv)x+randn(size(x)).*stdv(:);
-    prop = @(x)addnoise(x(1:obj.d(1)),1./sqrt(-1/2*obj.Z(1:obj.d(1))));
-    for i = 2:length(obj.d)
-      prop = @(x)cat(1,prop(x),normc(addnoise(x(sum(obj.d(1:i-1))+(1:obj.d(i))),3)));
-    end
-
-    m = 0;
-    x = zeros(n,sum(obj.d));
-
-    sample = obj.mu;
-    prevLike = -Inf;
-    while m<n+burnin
-      candidate = prop(sample);
-      %logLike = sum(bpLikelihood(candidate,mode,Y));
-      logLike = log(obj.pdf(candidate'));
-      r = logLike-prevLike;
-      if r>0 || exp(r)>rand
-        m = m+1
-        sample = candidate;
-        if m>burnin
-          x(m-burnin,:) = sample;
-        end
-        prevLike = logLike;
-      else
-
-      end
-    end
-  end
   
   function bingham = approximate(obj)
     x = obj.mode;
@@ -146,7 +77,7 @@ methods (Static)
 %     mnfbMarg = mnfb.marginal([2,3]);
 %     figure(4), plot(mnfbMarg)
 		
-    Z = [-2, -2, -30, 0];
+    Z = [-2, -2, -20, 0];
 		Ry = RotationMatrix('euler',[2.1847 0.9425 3.6825]); %rand(1,3)); %
 		Ry = blkdiag(Ry.R,1);
 		Rz = RotationMatrix.Rx(pi/3);
--- normalFisherBinghamDist.m
+++ normalFisherBinghamDist.m
@@ -12,6 +12,7 @@ properties
   % Fisher term (encompasses the mean)
   a = [0,0,0];
 	
+  %  log normalizing constant
   logC
 end
 
@@ -19,6 +20,7 @@ properties (Dependent)
   B
   mu
 	Z
+  mode
 end
 
 properties (Hidden, SetAccess=private)
@@ -36,14 +38,14 @@ methods
   end
 	
   function val = logNormConst(obj)
-    d = 1:obj.d(1);
-    g = obj.d(1)+1:sum(obj.d);
-    normalPart = obj.d(1)/2*log(2*pi)-1/2*log(det(-2*obj.B(d,d)));
+    i = 1:obj.d(1);
+    j = obj.d(1)+1:sum(obj.d);
+    normalPart = obj.d(1)/2*log(2*pi)-1/2*log(det(-2*obj.B(i,i)));
     if sum(obj.d)>obj.d(1)
 %       obj.C = obj.C*exp(logNormConstSP(sum(obj.d(2:end)),...
 %         zeros(sum(obj.d(2:end)),1),...
 %         obj.B(g,g)-obj.B(g,d)/obj.B(d,d)*obj.B(d,g)));
-      b = obj.B(g,g)-obj.B(g,d)/obj.B(d,d)*obj.B(d,g);
+      b = obj.B(j,j)-obj.B(j,i)/obj.B(i,i)*obj.B(i,j);
       [~,z] = eig(b);
       binghamPart = log(bingham_F(z(1)));
       if any(obj.a)
@@ -100,10 +102,54 @@ methods
     aCond = obj.a(g) + 2*obj.B(g,d)*points';
     BCond = obj.B(g,g);
     
-    if strcmp(class(obj),'normalFisherBinghamDist')
-      nfb = normalFisherBinghamDist('d',dCond,'a',aCond,'B',BCond);
-    elseif strcmp(class(obj),'mirroredNormalFisherBinghamDist')
+    if isa(obj,'mirroredNormalFisherBinghamDist')
       nfb = mirroredNormalFisherBinghamDist('d',dCond,'a',aCond,'B',BCond);
+    else
+      nfb = normalFisherBinghamDist('d',dCond,'a',aCond,'B',BCond);
+    end
+  end
+	
+	function mnfb = marginal(obj,d)
+    % d should be an nx1 array of indices
+    %
+    % We should check to make sure we're not trying to marginalize over a
+    % subset of linked directional variables. Do this.
+    
+    % this is a bit of a hack
+    % - these components are very small but not below eps
+		assert(~any(abs(obj.mu(obj.d(1)+1:end)/obj.mu(1))>10*eps),'There should be no Fisher components.')  
+		
+    % fix variable counts
+    dMarg = obj.d;
+    for i = 1:length(d)
+      j = find(d(i)<=cumsum(obj.d),1,'first');
+      dMarg(j) = dMarg(j)-1;
+    end
+		
+    % break into block matrix
+		d = sort(d);
+    g = setdiff(1:sum(obj.d),d);
+		mode = [-1/2*(obj.B(1:obj.d(1),1:obj.d(1))\obj.a(1:obj.d(1)));zeros(sum(obj.d(2:end)),1)];
+		modeMarg = mode(g);
+		
+		% compute second moment
+		n = 1000;
+		t = linspace(0,2*pi,n+1)'; t = t(1:end-1);
+		x = [cos(t),sin(t)];
+    if isa(obj,'mirroredNormalFisherBinghamDist')
+      b = mirroredNormalFisherBinghamDist('d',[0,2],'B',obj.B(d,d),'a',[0;0]);
+    else
+      b = normalFisherBinghamDist('d',[0,2],'B',obj.B(d,d),'a',[0;0]);
+    end
+		p = b.pdf(x);
+		m = x'*bsxfun(@times,x,p)*(t(2)-t(1));
+		
+    BMarg = obj.B(g,g)+2*obj.B(g,d)*m*obj.B(d,g);
+		
+    if isa(obj,'mirroredNormalFisherBinghamDist')
+      mnfb = mirroredNormalFisherBinghamDist('d',dMarg,'B',BMarg,'mu',modeMarg);
+    else
+      mnfb = normalFisherBinghamDist('d',dMarg,'B',BMarg,'mu',modeMarg);
     end
   end
 
@@ -129,10 +175,9 @@ methods
 		% labels
 		xlabel('x')
 		ylabel('y')
-	end
+  end
 	
 	function plot(obj)
-    clf
 		% generate sample points
     nThetas = 1000;
     thetas = linspace(0,2*pi,nThetas+1);
@@ -179,11 +224,13 @@ methods
       % fix axis
       ax(1) = im.Parent;
       ax(1).Position = [ax(1).Position(1),ax(1).Position(2),ax(1).Position(3)-0.05,ax(1).Position(4)-0.1];
-      axis equal tight xy
+      axis xy equal tight
+      
+      ax(1).Units = 'pixels';
+      pos = plotboxpos(ax(1));
       
       % add hue colorbar
-      pos = plotboxpos(im.Parent);
-      ax(2) = axes('Position',[pos(1)+pos(3),pos(2),0.05,pos(4)]);
+      ax(2) = axes('Units','pixels','Position',[pos(1)+pos(3),pos(2),30,pos(4)]);
       imagesc(permute(colormap('hsv'),[1,3,2]))
       ax(2).YAxisLocation='right';
       ax(2).XTick=[];
@@ -192,7 +239,7 @@ methods
       ylabel('angle (hue)')
       
       % add saturation colorbar
-      ax(3) = axes('Position',[pos(1),pos(2)+pos(4),pos(3),0.05]);
+      ax(3) = axes('Units','pixels','Position',[pos(1),pos(2)+pos(4),pos(3),30]);
       imagesc(hsv2rgb(bsxfun(@times,cat(3,ones(1,64),linspace(0,1,64),ones(1,64)),cat(3,linspace(0,1,64)',ones(64,1),ones(64,1)))))
       ax(3).XAxisLocation='top';
       ax(3).YTick=[];
@@ -216,7 +263,10 @@ methods
       like = reshape(like,nZs,nThetas);
 
       % plot on cylinder
-      plotOnCylinder(cos(T),sin(T),X,like)
+      zStd = 1/sqrt(-obj.B(1,1));
+      options = struct('camPos',obj.mode([2,3,1]).*[3,3,1]'+[0,0,4*zStd]',...
+        'cmap',bsxfun(@times,linspace(0.8,0,64)',[1,1,1]));
+      plotOnCylinder(cos(T),sin(T),X,like,options)
       
       xlabel('q_1')
       ylabel('q_2')
@@ -245,7 +295,7 @@ methods
       axis equal tight xy
       xlabel('x')
       ylabel('y')
-      colormap gray
+      colormap('gray')
 			
     elseif obj.d(1)==0 && obj.d(2)==2 % classic Bingham
       x = [cos(thetas(:)),sin(thetas(:))];
@@ -306,59 +356,99 @@ methods
 		val = obj.Z_;
 	end
   
-  function mode_hat = mode(obj)
-		% find the major axis of the Gaussian
-		m = obj.V(:,2);
-		
-		assert(m(1)~=0,'this does not work')
-			
-    % find the mean of the Gaussian
-    mu_hat = (-2*obj.B*[1;obj.V(2,1)/obj.V(1,1)])\obj.a*[1;obj.V(2,1)/obj.V(1,1)];
-    % to derive this, expand the expression a = -2 B mu
-    % assuming that mu(2)==m2/m2*mu(1) (it's on the Gaussian's minor axis)
+  function val = get.mode(obj)
+    val = nan(sum(obj.d),1);
+    val(1:obj.d(1)) = obj.mu(1:obj.d(1));
+    for i = 2:length(obj.d)
+      inds = obj.d(i-1)+(1:obj.d(i))';
+      assert(length(inds)==2,'Mode currently works only for 1D rotations.')
 
-    % find the intersection of the line (y-mu2)/(x-mu1)=m(2)/m(1) with the
-    % unit circle x^2+y^2=1 (y = +-sqrt(1-x^2)): http://mathworld.wolfram.com/Circle-LineIntersection.html
-    x1 = mu_hat(1);
-    x2 = mu_hat(1)+m(1);
-    y1 = mu_hat(2);
-    y2 = mu_hat(2)+m(2);
-    dx = x2-x1;
-    dy = y2-y1;
-    dr = sqrt(dx^2+dy^2);
-    D = x1*y2-x2*y1;
+      % find the mean of the Gaussian
+      % to derive this, expand the expression a = -2 B mu
+      % assuming that mu(2)==m2/m1*mu(1) (it's on the Gaussian's minor axis)
+      a_demeaned = -2*obj.B*(obj.mu.*[zeros(obj.d(1),1);ones(sum(obj.d(2:end)),1)]);
+      mu_hat = (-2*obj.B(inds,inds)*[1;obj.V(inds(2),inds(1))/obj.V(inds(1),inds(1))])\a_demeaned(inds)*[1;obj.V(inds(2),inds(1))/obj.V(inds(1),inds(1))];
 
-    mode_hat = {};
-    if dr^2-D^2>=0 % at least one intersection
-      mode_hat = cat(1,mode_hat,{[(D*dy+sign(dy)*dx*sqrt(dr^2-D^2))/dr^2;...
-        (-D*dx+abs(dy)*sqrt(dr^2-D^2))/dr^2]});
-    end
-    if dr^2-D^2>0 % two intersections
-      mode_hat = cat(1,mode_hat,{[(D*dy-sign(dy)*dx*sqrt(dr^2-D^2))/dr^2;...
-        (-D*dx-abs(dy)*sqrt(dr^2-D^2))/dr^2]});
-    end
+      mode_hat = {};
+      x1 = mu_hat(1);
+      y1 = mu_hat(2);
 
-    % and the minor axis
-    m = obj.V(:,1);
-    x2 = mu_hat(1)+m(1);
-    y2 = mu_hat(2)+m(2);
-    dx = x2-x1;
-    dy = y2-y1;
-    dr = sqrt(dx^2+dy^2);
-    D = x1*y2-x2*y1;
-    if dr^2-D^2>=0 % at least one intersection
-      mode_hat = cat(1,mode_hat,{[(D*dy+sign(dy)*dx*sqrt(dr^2-D^2))/dr^2;...
-        (-D*dx+abs(dy)*sqrt(dr^2-D^2))/dr^2]});
+      % find the intersection of the line (y-mu2)/(x-mu1)=m(2)/m(1) with the
+      % unit circle x^2+y^2=1:
+      % http://mathworld.wolfram.com/Circle-LineIntersection.html
+      % first with the major axis of the Gaussian
+      m = obj.V(inds,inds(2));
+      x2 = mu_hat(1)+m(1);
+      y2 = mu_hat(2)+m(2);
+      dx = x2-x1;
+      dy = y2-y1;
+      dr = sqrt(dx^2+dy^2);
+      D = x1*y2-x2*y1;
+      if dr^2-D^2>=0 % at least one intersection
+        mode_hat = cat(1,mode_hat,{[(D*dy+sign(dy)*dx*sqrt(dr^2-D^2))/dr^2;...
+          (-D*dx+abs(dy)*sqrt(dr^2-D^2))/dr^2]});
+      end
+      if dr^2-D^2>0 % two intersections
+        mode_hat = cat(1,mode_hat,{[(D*dy-sign(dy)*dx*sqrt(dr^2-D^2))/dr^2;...
+          (-D*dx-abs(dy)*sqrt(dr^2-D^2))/dr^2]});
+      end
+
+      % then the minor axis
+      m = obj.V(inds,inds(1));
+      x2 = mu_hat(1)+m(1);
+      y2 = mu_hat(2)+m(2);
+      dx = x2-x1;
+      dy = y2-y1;
+      dr = sqrt(dx^2+dy^2);
+      D = x1*y2-x2*y1;
+      if dr^2-D^2>=0 % at least one intersection
+        mode_hat = cat(1,mode_hat,{[(D*dy+sign(dy)*dx*sqrt(dr^2-D^2))/dr^2;...
+          (-D*dx+abs(dy)*sqrt(dr^2-D^2))/dr^2]});
+      end
+      if dr^2-D^2>0 % two intersections
+        mode_hat = cat(1,mode_hat,{[(D*dy-sign(dy)*dx*sqrt(dr^2-D^2))/dr^2;...
+          (-D*dx-abs(dy)*sqrt(dr^2-D^2))/dr^2]});
+      end
+
+      % pick the intersection with the greatest likelihood
+      llhs = obj.pdf(cat(2,repmat(val(1:obj.d(1))',length(mode_hat),1),cat(2,mode_hat{:})'));
+      [~,ind] = max(llhs);
+      val(inds) = mode_hat{ind};
     end
-    if dr^2-D^2>0 % two intersections
-      mode_hat = cat(1,mode_hat,{[(D*dy-sign(dy)*dx*sqrt(dr^2-D^2))/dr^2;...
-        (-D*dx-abs(dy)*sqrt(dr^2-D^2))/dr^2]});
+  end
+  
+  function x = sample(obj,n,burnin)
+    if nargin<3
+      burnin = 100;
     end
     
-    % pick the intersection with the greatest likelihood
-    llhs = obj.pdf(cat(2,mode_hat{:})');
-    [~,ind] = max(llhs);
-    mode_hat = mode_hat{ind};
+    addnoise = @(x,stdv)x+randn(size(x)).*stdv(:);
+    prop = @(x)addnoise(x(1:obj.d(1)),1./sqrt(-1/2*obj.Z(1:obj.d(1))));
+    for i = 2:length(obj.d)
+      prop = @(x)cat(1,prop(x),normc(addnoise(x(sum(obj.d(1:i-1))+(1:obj.d(i))),3)));
+    end
+
+    m = 0;
+    x = zeros(n,sum(obj.d));
+
+    sample = obj.mu;
+    prevLike = -Inf;
+    while m<n+burnin
+      candidate = prop(sample);
+      %logLike = sum(bpLikelihood(candidate,mode,Y));
+      logLike = log(obj.pdf(candidate'));
+      r = logLike-prevLike;
+      if r>0 || exp(r)>rand
+        m = m+1;
+        sample = candidate;
+        if m>burnin
+          x(m-burnin,:) = sample;
+        end
+        prevLike = logLike;
+      else
+
+      end
+    end
   end
 		
 end
